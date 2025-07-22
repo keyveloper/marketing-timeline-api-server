f270e7dbdd4fd7ff11401578037dac6c65ac3488
zen <didehdrb21@naver.com>
2025-07-22 20:58:22 +0900

    timeline-api-server build start!

---
+++ src/main/kotlin/org/example/marketingtimelineapiserver/MarketingTimelineApiServerApplication.kt
@@ -0,0 +1,11 @@
+package org.example.marketingtimelineapiserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MarketingTimelineApiServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<MarketingTimelineApiServerApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/marketingtimelineapiserver/MarketingTimelineApiServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.marketingtimelineapiserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MarketingTimelineApiServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
